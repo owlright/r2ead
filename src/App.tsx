80daf1c2b8a2d6e04ee7b3597e7398f456d7e3d6
owlright <jhsheng@mail.ustc.edu.cn>
2024-03-10 20:16:22 +0800

    style:art:(code): using vscode.typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 // import './App.css';
-import './Main.css';
-import Sidebar from './reader/Sidebar';
-import Titlebar from './reader/Titlebar';
-
+import "./Main.css";
+import Sidebar from "./reader/Sidebar";
+import Titlebar from "./reader/Titlebar";
 
 function Main() {
-  return (<div id="main">
-    <Titlebar />
-    <div id="divider"></div>
-    <div id="prev" className="arrow">‹</div>
-    <div id="viewer"></div>
-    <div id="next" className="arrow">›</div>
-  </div>);
+    return (
+        <div id="main">
+            <Titlebar />
+            <div id="divider"></div>
+            <div id="prev" className="arrow">
+                ‹
+            </div>
+            <div id="viewer"></div>
+            <div id="next" className="arrow">
+                ›
+            </div>
+        </div>
+    );
 }
 
 function App() {
-  return (
-    <div className="App">
-      <Sidebar />
-      <Main />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Sidebar />
+            <Main />
+        </div>
+    );
 }
 
 export default App;
